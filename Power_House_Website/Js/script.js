c4ebdcf60adc3845e28c2a2b831f9f78d32b0a10
Md Shabaz Ansari <91085427+Meshabaz@users.noreply.github.com>
2022-10-02 19:22:37 +0530

    JavaScript Added

---
+++ Power_House_Website/Js/script.js
@@ -0,0 +1,21 @@
+   // const div = document.querySelector('body')
+    // const sty = window.getComputedStyle(div);
+    // // const bi = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    // console.log(sty.backgroundImage)
+    // const image=sty.backgroundImage;
+    // const url= image.slice(5,-2);
+    // console.log(url);
+    var arr=["/img/bg.png.jpg","/img/bg1.png.jpg","/img/bg2.png.jpg","/img/bg3.jpg"]
+    var i=0;
+    document.body.style.backgroundImage = `url(${arr[3]})`;
+    function carousel(){
+        document.body.style.backgroundImage = `url(${arr[i]})`;
+        i++;
+        if(i>=arr.length){
+            i=0;
+        }
+    }
+    setInterval("carousel()",3000);
+
+
+
